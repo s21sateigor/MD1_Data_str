a118a994f7563129f091caa473e90a4c78bdb37e
Igors Saters <s21sateigor@venta.lv>
2023-03-25 16:31:43 +0200

    Add MyNodeQ class

--- src/MyNodeQ.java
+++ src/MyNodeQ.java
@@ -1,2 +1,31 @@
-public class MyNodeQ {
+public class MyNodeQ<T>
+{
+    private T element;
+    private MyNodeQ<T> next;
+
+    public MyNodeQ(T elemet){
+        this.element = elemet;
+        this.next = null;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public MyNodeQ<T> getNext() {
+        return next;
+    }
+
+    public void setNext(MyNodeQ<T> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
 }
